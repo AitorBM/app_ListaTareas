2a838c8c75d43b3d026ae4114f0175573442d5c5
AitorBM <8fprogmm03@a224p03.arriagainfo.local>
2016-12-13 22:03:46 +0100

    Empezando, diseño casi terminado

---
+++ app_ListaTareas/app_ListaTareas/MiCeldaTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  MiCeldaTableViewCell.swift
+//  app_ListaTareas
+//
+//  Created by  on 13/12/16.
+//  Copyright © 2016 Egibide. All rights reserved.
+//
+
+import UIKit
+
+class MiCeldaTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var etiqueta: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- app_ListaTareas/app_ListaTareas/ViewController.swift
+++ app_ListaTareas/app_ListaTareas/ViewController.swift
@@ -10,9 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var posicion: String!
+    
+    @IBOutlet weak var etiqueta: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if let p = posicion {
+            etiqueta.text = p
+        } else {
+            etiqueta.text = ":("
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
